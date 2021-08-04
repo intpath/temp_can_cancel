b7a70088af40923b0ef43f0c1b72ba528c39523b
Hayder <hadermusc@gmai.com>
2021-08-04 12:17:44 +0000

    add epends

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'car_repair_maintenance_service'],
 
     # always loaded
     'data': [
